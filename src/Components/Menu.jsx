69aa5173bd52df4a9812a3e9493cf43275b4c3b4
Malik-444 <talhahusnain475@gmail.com>
2024-01-16 23:53:39 +0500

    fix issue

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -16,14 +16,18 @@ const Menu = () => {
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+  const menuClose = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <div className="relative">
       <IoMdMenu size={24} onClick={toggleMenu} />
       {menuOpen && (
-        <div className="absolute top-8 left-0 w-max p-3 bg-base-300 menu flex items-start gap-y-3">
+        <div className="absolute top-8 -left-5 w-max p-3 bg-base-300 menu flex items-start gap-y-3">
           {data.map((item, idx) => {
             return (
-              <div key={idx}>
+              <div key={idx} onClick={menuClose}>
                 <Link
                   href={item.link}
                   className="text-xl gap-y-4 font-medium hover:text-accent transition ease-in-out duration-150 delay-150"
