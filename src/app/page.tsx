e89f8dfaab16e14786bf019f248534dd911f4aea
Malik-444 <talhahusnain475@gmail.com>
2024-01-17 12:52:35 +0500

    Hero Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
+import Hero from "@/Components/Hero";
 
 export default function Home() {
   return (
     <>
-      <div className="flex bg-[#f6f6f6]">
-        <div className="flex flex-col  md:flex-[2]  justify-center items-center gap-y-3 ">
-          <span className="text-2xl font-normal capitalize">Trend Design!</span>
-          <h1 className="text-2xl ml-4 md:text-6xl font-bold uppercase">
-            Women's Wear
-          </h1>
-          <span className=" hidden md:flex bg-base-100 shadow-lg text-base px-3 py-1 font-light w-max rounded-2xl">
-            20% discount over 40$ purchase use Code 0145698
-          </span>
-          <button className="btn w-max bg-accent mt-5  text-xl md:text-2xl hover:bg-accent flex ">
-            shop now
-          </button>
-        </div>
-        <div className="flex flex-1">
-          <Image
-            src={"/h-1.jpg"}
-            alt=""
-            className="w-screen "
-            width={500}
-            height={500}
-          />
-        </div>
-      </div>
+      <Hero />
     </>
   );
 }
