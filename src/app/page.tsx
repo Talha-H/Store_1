3b3692c4523ba80396e87247a87cedd844ef1ea1
Malik-444 <talhahusnain475@gmail.com>
2024-01-17 12:49:07 +0500

    Pages add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="flex bg-[#f6f6f6] ">
+      <div className="flex bg-[#f6f6f6]">
         <div className="flex flex-col  md:flex-[2]  justify-center items-center gap-y-3 ">
           <span className="text-2xl font-normal capitalize">Trend Design!</span>
           <h1 className="text-2xl ml-4 md:text-6xl font-bold uppercase">
