44d9fa40b54fd8e5e071aa7d8b2e95cf2395b58c
Malik-444 <talhahusnain475@gmail.com>
2024-01-17 14:07:03 +0500

    Char Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 import Hero from "@/Components/Hero";
+import Char from "@/Components/Char";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <div>
-        
-      </div>
+      <Char />
     </>
   );
 }
