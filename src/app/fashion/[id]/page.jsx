2e45588f971a2a574a77fce4b3da002d20316a9c
Malik-444 <talhahusnain475@gmail.com>
2024-01-22 23:32:00 +0500

    Loading fashion

--- src/app/fashion/[id]/page.jsx
+++ src/app/fashion/[id]/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = ({ id }) => {
   return (
     <div>
-      <h1>Page {id}</h1>
+      <h1></h1>
     </div>
   );
 };
---
+++ src/app/fashion/loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center my-40">
+      <span className="loading loading-infinity loading-lg"></span>
+      <span className="loading loading-infinity loading-lg"></span>
+    </div>
+  );
+};
+
+export default loading;
