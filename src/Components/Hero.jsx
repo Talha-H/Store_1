a22149c64dba427cb4954ee1d74730ed174da7aa
Malik-444 <talhahusnain475@gmail.com>
2024-01-22 14:01:58 +0500

    framer-motion not working

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,36 +1,38 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react'
-import Image from 'next/image'
-import Social from "@/Components/Social"
+import React from "react";
+import Image from "next/image";
+import Social from "@/Components/Social";
 
 const Hero = () => {
   return (
     <div className="flex bg-[#f6f6f6]">
-      <div className='flex justify-center items-center'><Social/></div>
-        <div className="flex flex-col  md:flex-[2]  justify-center items-center gap-y-3 ">
-          <span className="text-2xl font-normal capitalize">Trend Design!</span>
-          <h1 className="text-2xl ml-4 md:text-6xl font-bold uppercase">
-            Women's Wear
-          </h1>
-          <span className=" hidden md:flex bg-base-100 shadow-lg text-base px-3 py-1 font-light w-max rounded-2xl">
-            20% discount over 40$ purchase use Code 0145698
-          </span>
-          <button className="btn w-max bg-accent mt-5  text-xl md:text-2xl hover:bg-accent flex ">
-            shop now
-          </button>
-        </div>
-       
-        <div className="flex flex-1">
-          <Image
-            src={"/h-1.jpg"}
-            alt=""
-            className="w-screen "
-            width={500}
-            height={500}
-          />
-        </div>
+      <div className="flex justify-center items-center">
+        <Social />
       </div>
-  )
-}
+      <div className="flex flex-col  md:flex-[2]  justify-center items-center gap-y-3 ">
+        <span className="text-2xl font-normal capitalize">Trend Design!</span>
+        <h1 className="text-2xl ml-4 md:text-6xl font-bold uppercase">
+          Women's Wear
+        </h1>
+        <span className=" hidden md:flex bg-base-100 shadow-lg text-base px-3 py-1 font-light w-max rounded-2xl">
+          20% discount over 40$ purchase use Code 0145698
+        </span>
+        <button className="btn w-max bg-accent mt-5  text-xl md:text-2xl hover:bg-accent flex ">
+          shop now
+        </button>
+      </div>
 
-export default Hero
+      <div className="flex flex-1">
+        <Image
+          src={"/h-1.jpg"}
+          alt=""
+          className="w-screen "
+          width={500}
+          height={500}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
