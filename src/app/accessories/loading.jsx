bda1b182c363d2d32da904fadc3a3f344eb46ac0
Malik-444 <talhahusnain475@gmail.com>
2024-01-24 14:49:06 +0500

    lit change in loading

--- src/app/accessories/loading.jsx
+++ src/app/accessories/loading.jsx
@@ -3,10 +3,9 @@ import React from "react";
 const loading = () => {
   return (
     <div className="flex justify-center items-center my-40">
-      <span className="loading loading-dots loading-lg"></span>
-      <span className="loading loading-dots loading-lg"></span>
-      <span className="loading loading-dots loading-lg"></span>
-      <span className="loading loading-dots loading-lg"></span>
+      <span className="loading loading-infinity loading-lg"></span>
+      <span className="loading loading-infinity loading-lg"></span>
+      <span className="loading loading-infinity loading-lg"></span>
     </div>
   );
 };
