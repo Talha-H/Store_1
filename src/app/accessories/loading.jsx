36a2ecc97f202869be1c4fceb5e207b0474868d5
Malik-444 <talhahusnain475@gmail.com>
2024-01-24 14:47:10 +0500

    Loading Accessories PAge

---
+++ src/app/accessories/loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center my-40">
+      <span className="loading loading-dots loading-lg"></span>
+      <span className="loading loading-dots loading-lg"></span>
+      <span className="loading loading-dots loading-lg"></span>
+      <span className="loading loading-dots loading-lg"></span>
+    </div>
+  );
+};
+
+export default loading;
