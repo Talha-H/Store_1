89b0e623b0f1e0e30f3af8712c6cb79cad637977
Malik-444 <talhahusnain475@gmail.com>
2024-01-29 21:58:39 +0500

    Fetch from env

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 const fetchData = async () => {
   try {
-    const response = await fetch("https://fakestoreapi.com/products");
+    const response = await fetch(process.env.API_URL);
     const data = await response.json();
     console.log(data);
     return data;
