97078a3a9607934122770bf261842679b3ce06aa
Malik-444 <talhahusnain475@gmail.com>
2024-02-12 12:41:16 +0500

    prismMongoDb Connect

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,9 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 
+const prisma = PrismaClient();
 export const authOPtion = {
-}
+  adapter: PrismaAdapter(prisma),
+};
