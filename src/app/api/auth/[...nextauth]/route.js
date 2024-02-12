b37a2bfb4586587d065064010c18a101e9b78209
Malik-444 <talhahusnain475@gmail.com>
2024-02-12 22:56:53 +0500

    LogIn Page

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,8 +2,9 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
-const prisma = PrismaClient();
+const prisma = new PrismaClient();
 export const authOPtion = {
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -12,8 +13,30 @@ export const authOPtion = {
       credentials: {
         username: { label: "name", type: "text", placeholder: "Talha" },
         password: { label: "password", type: "password" },
+        email: { label: "email", type: "email" },
       },
-      authorize: async (credentials, req) => {},
+      authorize: async (credentials) => {
+        if (!credentials.username || !credentials.password) {
+          throw new Error("Username and Password are required");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user) {
+          return null;
+        }
+        // If the user exists, we verify the password using bcrypt compareSync() function
+        const passwordMatched = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        if (!passwordMatched) {
+          return null;
+        }
+        return user;
+      },
     }),
   ],
   session: {
